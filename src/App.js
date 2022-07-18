0c833f80d35bf597bcd86c45efe2dc0dc1ac97c5
Roberto Luna <robertocgw@gmail.com>
2022-07-18 14:51:58 -0300

    Recomeçando pois me perdi no processo, tentar entregar na recuperação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Table from './components/Table';
 import './App.css';
-import PlanetProvider from './context/PlanetProvider';
-import Filters from './components/Filters';
 
 function App() {
   return (
-    <PlanetProvider>
-      <Filters />
-      <Table />
-    </PlanetProvider>
+    <div>
+      alo
+    </div>
   );
 }
 
--- src/context/contextPlanet.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const contextPlanet = createContext();
-
-export default contextPlanet;
