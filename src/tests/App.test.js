cc9bbe711f2d6b44fbba4a7d3b48d96d34c9abd3
Roberto Luna <robertocgw@gmail.com>
2022-07-19 12:20:45 -0300

    Requisito 5 funcionando, aprovação com dificuldade, revisitar projeto e praticar mais o assunto.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { cleanup, render, screen, waitFor, act, fireEvent } from '@testing-library/react';
+import MockApi from './MockApi'
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testa se a página é renderizada corretamente' ,() => {
+  beforeEach(async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(MockApi)
+    })
+    await act(async () => {
+      render(<App />)
+    })
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    cleanup();
+  })
+
+  test('Testa se o handleChange funciona corretamente', async () => {
+    expect(fetch).toHaveBeenCalled()
+    expect(await screen.findAllByRole('row')).toHaveLength(11)
+
+    const Filter = screen.getByTestId('name-filter');
+    userEvent.type(Filter, 'Tatooine')
+    expect(await screen.findAllByRole('row')).toHaveLength(2)
+  });
+
+  test('Testa se o filtro "igual a" está funcionando', async () => {
+    expect(fetch).toHaveBeenCalled()
+    expect(await screen.findAllByRole('row')).toHaveLength(11)
+
+    const valueFilter = screen.getByTestId('value-filter');
+    const btnFiltrar = screen.getByTestId('button-filter');
+
+    fireEvent.change(screen.getByTestId('comparison-filter'), {target: { value: "igual a"} });
+
+    userEvent.type(valueFilter, '1000000')
+    userEvent.click(btnFiltrar);
+
+    expect(screen.getByText(/0/i)).toBeInTheDocument();
+
+  });
 });
